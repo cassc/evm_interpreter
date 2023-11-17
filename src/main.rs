b04f9a5a65a14056352bb4d6873e2abbe9527355
chen <chencassc@gmail.com>
2023-11-17 10:33:01 +0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -4,49 +4,108 @@ use runner::{execute_test_suite, run_evm, ResultWithTrace};
 
 pub mod inspector;
 pub mod runner;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use serde_json::json;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
-    /// A hex string representing a contract runtime binary code
-    #[clap(long)]
-    #[clap(long, conflicts_with = "test_json")]
-    bytecode: Option<String>,
+    #[clap(subcommand)]
+    command: Commands,
+}
 
-    /// An optional hex encoded transaction data
-    #[clap(long)]
-    #[clap(long, conflicts_with = "test_json")]
-    input: Option<String>,
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Executes the EVM interpreter with provided options
+    Execute {
+        /// A hex string representing a contract runtime binary code
+        #[clap(long, conflicts_with = "test_json")]
+        bytecode: Option<String>,
 
-    /// If provided, print stack traces to stdout
-    #[clap(long)]
-    pprint: bool,
+        /// An optional hex encoded transaction data
+        #[clap(long, conflicts_with = "test_json")]
+        input: Option<String>,
 
-    /// If provided, output as JSON to this file
-    #[clap(long)]
-    output: Option<String>,
+        /// If provided, print stack traces to stdout
+        #[clap(long)]
+        pprint: bool,
 
-    /// If provided, use the ethtest JSON file the input
-    #[clap(long)]
-    test_json: Option<String>,
+        /// If provided, output as JSON to this file
+        #[clap(long)]
+        output: Option<String>,
 
-    /// Maximum number of test files to run, valid when using with --test-json
-    #[clap(long, default_value_t = 10)]
-    limit: usize,
+        /// If provided, use the ethtest JSON file as the input
+        #[clap(long)]
+        test_json: Option<String>,
+
+        /// Maximum number of test files to run, valid when using with --test-json
+        #[clap(long, default_value_t = 10)]
+        limit: usize,
+    },
+    /// Compares the output of two EVM interpreters
+    Compare {
+        /// Path to another EVM interpreter executable
+        #[clap(long)]
+        executable: String,
+
+        /// A path which contains ethtest JSON files
+        #[clap(long)]
+        test_json: String,
+
+        /// Maximum number of test files to run, valid when using with --test-json
+        #[clap(long, default_value_t = 10)]
+        limit: usize,
+    },
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    let code = cli.bytecode;
-    let input = cli.input;
+    match &cli.command {
+        Commands::Execute {
+            bytecode,
+            input,
+            pprint,
+            output,
+            test_json,
+            limit,
+        } => {
+            // Logic for the 'execute' subcommand
+            execute_evm(
+                bytecode.clone(),
+                input.clone(),
+                *pprint,
+                output.clone(),
+                test_json.clone(),
+                *limit,
+            );
+        }
+        Commands::Compare {
+            executable,
+            test_json,
+            limit,
+        } => {
+            // Logic for the 'compare' subcommand
+            compare_interpreters(executable.into(), test_json.clone(), *limit);
+        }
+    }
+}
+
+fn execute_evm(
+    bytecode: Option<String>,
+    input: Option<String>,
+    pprint: bool,
+    output: Option<String>,
+    test_json: Option<String>,
+    limit: usize,
+) {
+    let code = bytecode;
+    let input = input;
 
     let results = {
-        if let Some(test_json) = cli.test_json {
+        if let Some(test_json) = test_json {
             let path = Path::new(&test_json);
-            execute_test_suite(&path, cli.limit).unwrap()
+            execute_test_suite(path, limit).unwrap()
         } else {
             let code = code.expect("Contract code should be provided");
             run_evm(code, input).unwrap()
@@ -60,7 +119,7 @@ fn main() {
         traces,
     } in results.iter()
     {
-        if cli.pprint {
+        if pprint {
             traces.iter().for_each(|trace| {
                 trace.pprint();
             });
@@ -75,8 +134,12 @@ fn main() {
         println!();
     }
 
-    if let Some(output_path) = &cli.output {
+    if let Some(output_path) = output {
         let output_data = json!({"results": results});
         fs::write(output_path, output_data.to_string()).expect("Unable to write to file");
     }
+}
+
+fn compare_interpreters(executable: String, test_json: String, limit: usize) {
+    let path = Path::new(&test_json);
 }
--- src/runner.rs
+++ src/runner.rs
@@ -370,6 +370,213 @@ pub fn execute_test_suite(path: &Path, limit: usize) -> Result<Vec<ResultWithTra
     Ok(results)
 }
 
+/// Load a ethtest test suite json and execute
+pub fn compare_test_suite(
+    alt_evm_path: String,
+    test_json_path: &Path,
+    limit: usize,
+) -> Result<Vec<ResultWithTrace>> {
+    let mut results = vec![];
+    if test_json_path.is_dir() {
+        let mut json_files: Vec<PathBuf> = {
+            WalkDir::new(test_json_path)
+                .into_iter()
+                .filter_map(|e| e.ok())
+                .filter(|e| e.file_name().to_string_lossy().ends_with(".json"))
+                .map(DirEntry::into_path)
+                .collect()
+        };
+
+        let mut rng = thread_rng();
+        json_files.shuffle(&mut rng);
+        let json_files: Vec<PathBuf> = json_files.iter().take(limit).cloned().collect();
+
+        for path in json_files {
+            let mut r = compare_test_suite(alt_evm_path.clone(), &path, 1)?;
+            results.append(&mut r);
+        }
+        return Ok(results);
+    }
+
+    println!("Processing {}", test_json_path.display());
+
+    let s = std::fs::read_to_string(test_json_path)?;
+    let suite: TestSuite = serde_json::from_str(&s)?;
+
+    let map_caller_keys: HashMap<_, _> = [
+        (
+            b256!("45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8"),
+            address!("a94f5374fce5edbc8e2a8697c15331677e6ebf0b"),
+        ),
+        (
+            b256!("c85ef7d79691fe79573b1a7064c19c1a9819ebdbd1faaab1a8ec92344438aaf4"),
+            address!("cd2a3d9f938e13cd947ec05abc7fe734df8dd826"),
+        ),
+        (
+            b256!("044852b2a670ade5407e78fb2863c51de9fcb96542a07186fe3aeda6bb8a116d"),
+            address!("82a978b3f5962a5b0957d9ee9eef472ee55b42f1"),
+        ),
+        (
+            b256!("6a7eeac5f12b409d42028f66b0b2132535ee158cfda439e3bfdd4558e8f4bf6c"),
+            address!("c9c5a15a403e41498b6f69f6f89dd9f5892d21f7"),
+        ),
+        (
+            b256!("a95defe70ebea7804f9c3be42d20d24375e2a92b9d9666b832069c5f3cd423dd"),
+            address!("3fb1cd2cd96c6d5c0b5eb3322d807b34482481d4"),
+        ),
+        (
+            b256!("fe13266ff57000135fb9aa854bbfe455d8da85b21f626307bf3263a0c2a8e7fe"),
+            address!("dcc5ba93a1ed7e045690d722f2bf460a51c61415"),
+        ),
+    ]
+    .into();
+
+    for (_name, unit) in suite.0 {
+        // Create database and insert cache
+        let mut cache_state = revm::CacheState::new(false);
+        for (address, info) in unit.pre {
+            let acc_info = revm::primitives::AccountInfo {
+                balance: info.balance,
+                code_hash: keccak256(&info.code),
+                code: Some(Bytecode::new_raw(info.code)),
+                nonce: info.nonce,
+            };
+            cache_state.insert_account_with_storage(address, acc_info, info.storage);
+        }
+
+        let mut env = Env::default();
+        // for mainnet
+        env.cfg.chain_id = 1;
+        // env.cfg.spec_id is set down the road
+
+        // block env
+        env.block.number = unit.env.current_number;
+        env.block.coinbase = unit.env.current_coinbase;
+        env.block.timestamp = unit.env.current_timestamp;
+        env.block.gas_limit = unit.env.current_gas_limit;
+        env.block.basefee = unit.env.current_base_fee.unwrap_or_default();
+        env.block.difficulty = unit.env.current_difficulty;
+        // after the Merge prevrandao replaces mix_hash field in block and replaced difficulty opcode in EVM.
+        env.block.prevrandao = Some(unit.env.current_difficulty.to_be_bytes().into());
+        // EIP-4844
+        if let (Some(parent_blob_gas_used), Some(parent_excess_blob_gas)) = (
+            unit.env.parent_blob_gas_used,
+            unit.env.parent_excess_blob_gas,
+        ) {
+            env.block
+                .set_blob_excess_gas_and_price(calc_excess_blob_gas(
+                    parent_blob_gas_used.to(),
+                    parent_excess_blob_gas.to(),
+                ));
+        }
+
+        // tx env
+        let pk = unit.transaction.secret_key;
+        env.tx.caller = map_caller_keys
+            .get(&pk)
+            .copied()
+            .context("unknown caller private key")?;
+        env.tx.gas_price = unit
+            .transaction
+            .gas_price
+            .or(unit.transaction.max_fee_per_gas)
+            .unwrap_or_default();
+        env.tx.gas_priority_fee = unit.transaction.max_priority_fee_per_gas;
+        // EIP-4844
+        env.tx.blob_hashes = unit.transaction.blob_versioned_hashes;
+        env.tx.max_fee_per_blob_gas = unit.transaction.max_fee_per_blob_gas;
+
+        // post and execution
+        for (spec_name, tests) in unit.post {
+            if matches!(
+                spec_name,
+                SpecName::ByzantiumToConstantinopleAt5
+                    | SpecName::Constantinople
+                    | SpecName::Unknown
+            ) {
+                continue;
+            }
+
+            env.cfg.spec_id = spec_name.to_spec_id();
+
+            for (index, test) in tests.into_iter().enumerate() {
+                env.tx.gas_limit = unit.transaction.gas_limit[test.indexes.gas].saturating_to();
+
+                env.tx.data = unit
+                    .transaction
+                    .data
+                    .get(test.indexes.data)
+                    .unwrap()
+                    .clone();
+                env.tx.value = unit.transaction.value[test.indexes.value];
+
+                env.tx.access_list = unit
+                    .transaction
+                    .access_lists
+                    .get(test.indexes.data)
+                    .and_then(Option::as_deref)
+                    .unwrap_or_default()
+                    .iter()
+                    .map(|item| {
+                        (
+                            item.address,
+                            item.storage_keys
+                                .iter()
+                                .map(|key| U256::from_be_bytes(key.0))
+                                .collect::<Vec<_>>(),
+                        )
+                    })
+                    .collect();
+
+                let to = match unit.transaction.to {
+                    Some(add) => TransactTo::Call(add),
+                    None => TransactTo::Create(CreateScheme::Create),
+                };
+                env.tx.transact_to = to;
+
+                let mut cache = cache_state.clone();
+                cache.set_state_clear_flag(SpecId::enabled(
+                    env.cfg.spec_id,
+                    revm::primitives::SpecId::SPURIOUS_DRAGON,
+                ));
+                let mut state = revm::db::State::builder()
+                    .with_cached_prestate(cache)
+                    .with_bundle_update()
+                    .build();
+                let mut evm = revm::new();
+                evm.database(&mut state);
+                evm.env = env.clone();
+
+                let mut traces = vec![];
+                let inspector = TraceInspector {
+                    traces: &mut traces,
+                };
+
+                let result = evm.inspect_commit(inspector);
+                let mut success = false;
+                let mut output = Bytes::new();
+                if let Ok(result) = result {
+                    success = result.is_success();
+                    if let Some(o) = result.output() {
+                        output = o.to_owned();
+                    }
+                }
+
+                let traces = traces.to_owned();
+                results.push(ResultWithTrace {
+                    id: format!("{}_{:?}_{}", test_json_path.display(), spec_name, index),
+                    success,
+                    output,
+                    traces,
+                });
+
+                // NOTE Test cases ignored
+            }
+        }
+    }
+    Ok(results)
+}
+
 #[cfg(test)]
 mod tests {
 
